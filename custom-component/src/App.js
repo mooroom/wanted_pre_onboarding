aa2ed0e2b8bd3262f4220f200934c1144aeafda1
mooroom <61869246+mooroom@users.noreply.github.com>
2022-02-11 16:32:03 +0900

    Global Nav

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Toggle from "./component/Toggle";
 import Modal from "./component/Modal";
@@ -6,15 +7,60 @@ import Tag from "./component/Tag";
 import AutoComplete from "./component/AutoComplete";
 import ClickToEdit from "./component/ClickToEdit";
 
+import styled from "styled-components";
+
+const ComponentsList = [
+  "toggle",
+  "modal",
+  "tab",
+  "tag",
+  "autoComplete",
+  "clickToEdit",
+];
+
+const ComponentsNavBlock = styled.nav`
+  position: fixed;
+  top: 50px;
+  left: 50px;
+
+  li {
+    cursor: pointer;
+    padding: 10px;
+    font-weight: bold;
+    color: var(--text-gray);
+
+    &:active {
+      color: var(--primary);
+    }
+  }
+`;
+
+const ComponentsNav = ({ setComopnent }) => {
+  return (
+    <ComponentsNavBlock>
+      <ul>
+        {ComponentsList.map((component, idx) => (
+          <li key={idx} onClick={() => setComopnent(component)}>
+            {component}
+          </li>
+        ))}
+      </ul>
+    </ComponentsNavBlock>
+  );
+};
+
 function App() {
+  const [component, setComopnent] = useState("toggle");
+
   return (
     <div className="App">
-      {/* <Toggle /> */}
-      {/* <Modal /> */}
-      {/* <Tab /> */}
-      {/* <Tag /> */}
-      {/* <AutoComplete /> */}
-      <ClickToEdit />
+      <ComponentsNav setComopnent={setComopnent} />
+      {component === "toggle" && <Toggle />}
+      {component === "modal" && <Modal />}
+      {component === "tab" && <Tab />}
+      {component === "tag" && <Tag />}
+      {component === "autoComplete" && <AutoComplete />}
+      {component === "clickToEdit" && <ClickToEdit />}
     </div>
   );
 }
